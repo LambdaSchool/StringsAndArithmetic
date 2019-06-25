0fc5ff1759b3a5008e67079655963656432b92d3
Johnny Hicks <johnnydhicks@gmail.com>
2019-06-24 19:06:03 -0600

    added missing variables

--- StringsArithmetic.playground/Pages/Arithmetic.xcplaygroundpage/Contents.swift
+++ StringsArithmetic.playground/Pages/Arithmetic.xcplaygroundpage/Contents.swift
@@ -3,7 +3,13 @@
  ## Calorie Goal
  Below you have constants for the number of calories you burned each day over the last week. Using simple arithmetic find the average number of calories burned. Add 100 calories to the average and use string interpolation to print out "Your new move goal for the week is [your calculated goal].
  */
-
+let mondayCalories = 340
+let tuesdayCalories = 420
+let wednedayCalories = 200
+let thursdayCalories = 625
+let fridayCalories = 500
+let saturdayCalories = 1_090
+let sundayCalories = 300
 
 
 
